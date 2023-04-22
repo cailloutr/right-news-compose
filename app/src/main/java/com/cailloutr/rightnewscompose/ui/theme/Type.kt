ad7beee9c1b5583e6f7fdb2d15ecfdbc1baface0
cailloutr <caio.trocilo@gmail.com>
2023-04-22 16:21:06 -0300

    Initial setup

--- app/src/main/java/com/cailloutr/rightnewscompose/ui/theme/Type.kt
+++ app/src/main/java/com/cailloutr/rightnewscompose/ui/theme/Type.kt
@@ -2,33 +2,149 @@ package com.cailloutr.rightnewscompose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.cailloutr.rightnewscompose.R
+
+private val fontFamilyMontserrat = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.montserrat_regular
+        ),
+        Font(
+            resId = R.font.montserrat_bold,
+            weight = FontWeight.Bold
+        ),
+        Font(
+            resId = R.font.montserrat_semi_bold,
+            weight = FontWeight.SemiBold
+        )
+    )
+)
+
+private val fontFamilyNunito = FontFamily(
+    listOf(
+        Font(
+            resId = R.font.nunito_regular
+        ),
+        Font(
+            resId = R.font.nunito_light,
+            weight = FontWeight.Light
+        ),
+        Font(
+            resId = R.font.nunito_semi_bold,
+            weight = FontWeight.SemiBold
+        )
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = fontFamilyMontserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+        fontSize = 57.sp,
+        lineHeight = 64.sp,
+        letterSpacing = (-0.25).sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 45.sp,
+        lineHeight = 52.sp,
+        letterSpacing = 0.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 32.sp,
+        lineHeight = 40.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+        lineHeight = 36.sp,
+        letterSpacing = 0.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamilyMontserrat,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+    titleMedium = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (0.15).sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = (0.1).sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = (0.1).sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = fontFamilyMontserrat,
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = (0.5).sp
+    ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamilyMontserrat,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        letterSpacing = (0.5).sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = fontFamilyNunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = (0.5).sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = fontFamilyNunito,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = (0.25).sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = (0.4).sp
     )
-    */
 )
