91259a30d894d4cd1d462e030597b905574ff7e3
cailloutr <caio.trocilo@gmail.com>
2023-05-01 10:59:49 -0300

    Refactoring

--- app/src/main/java/com/cailloutr/rightnewscompose/util/DateUtil.kt
+++ app/src/main/java/com/cailloutr/rightnewscompose/util/DateUtil.kt
@@ -2,13 +2,13 @@ package com.cailloutr.rightnewscompose.util
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import java.text.SimpleDateFormat
 import java.time.Instant
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 object DateUtil {
 
-    @RequiresApi(Build.VERSION_CODES.O)
+    /*@RequiresApi(Build.VERSION_CODES.O)
     fun getFormattedDate(date: String): String {
         val instantDate = Date.from(Instant.parse(date))
         val simpleDateFormat = SimpleDateFormat(
@@ -16,5 +16,16 @@ object DateUtil {
             Locale.getDefault()
         )
         return simpleDateFormat.format(instantDate)
+    }*/
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun getFormattedDate(date: String): String {
+        val instantDate = Date.from(Instant.parse(date))
+        return DateTimeFormatter
+            .ofPattern(
+                "EEE, d MMM, yyyy - HH:mm - z",
+                Locale.getDefault()
+            )
+            .format(Instant.ofEpochMilli(instantDate.time))
     }
 }
