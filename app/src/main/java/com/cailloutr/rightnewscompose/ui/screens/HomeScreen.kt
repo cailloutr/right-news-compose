f07eca1403348e647dd66f364872bc1772533a66
cailloutr <caio.trocilo@gmail.com>
2023-05-02 11:55:10 -0300

    Implements conditions and animation for Appbar and BottomNavigationBar visibility

--- app/src/main/java/com/cailloutr/rightnewscompose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/cailloutr/rightnewscompose/ui/screens/HomeScreen.kt
@@ -2,6 +2,7 @@ package com.cailloutr.rightnewscompose.ui.screens
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,15 +10,18 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.cailloutr.rightnewscompose.R
-import com.cailloutr.rightnewscompose.ui.BottomNavigationBar
+import com.cailloutr.rightnewscompose.navigation.BottomBarScreens
+import com.cailloutr.rightnewscompose.ui.components.BottomNavigationBar
 import com.cailloutr.rightnewscompose.navigation.HomeNavGraph
 import com.cailloutr.rightnewscompose.ui.components.RightNewsMainAppbar
 import com.cailloutr.rightnewscompose.ui.theme.RightNewsComposeTheme
@@ -30,14 +34,34 @@ fun HomeScreen(navController: NavHostController = rememberNavController()) {
     val enterAlwaysScrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(appBarState)
     val scrollBehavior = remember { enterAlwaysScrollBehavior }
 
+    val screens = listOf(
+        BottomBarScreens.Main,
+        BottomBarScreens.Favorite,
+        BottomBarScreens.Profile
+    )
+
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val bottomBarDestination = screens.any { it.route == currentDestination?.route }
+
     Scaffold(
         topBar = {
-            RightNewsMainAppbar(
-                title = stringResource(id = R.string.app_name),
-                scrollBehavior = scrollBehavior
-            )
+            AnimatedVisibility(visible = bottomBarDestination) {
+                RightNewsMainAppbar(
+                    title = stringResource(id = R.string.app_name),
+                    scrollBehavior = scrollBehavior
+                )
+            }
         },
-        bottomBar = { BottomNavigationBar(navController = navController) },
+        bottomBar = {
+            AnimatedVisibility(visible = bottomBarDestination) {
+                BottomNavigationBar(
+                    navController = navController,
+                    currentDestination = currentDestination,
+                )
+            }
+        },
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
     ) { paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
