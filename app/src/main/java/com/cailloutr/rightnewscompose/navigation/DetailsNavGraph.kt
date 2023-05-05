7bc4070e8bac492385c014554f29c97d9216e736
cailloutr <caio.trocilo@gmail.com>
2023-05-05 00:15:21 -0300

    Refactoring

---
+++ app/src/main/java/com/cailloutr/rightnewscompose/navigation/DetailsNavGraph.kt
@@ -0,0 +1,57 @@
+package com.cailloutr.rightnewscompose.navigation
+
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.UriHandler
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import androidx.navigation.navigation
+import com.cailloutr.rightnewscompose.R
+import com.cailloutr.rightnewscompose.extensions.fromRouteId
+import com.cailloutr.rightnewscompose.extensions.shareLinkIntent
+import com.cailloutr.rightnewscompose.ui.screens.detailscreen.DetailsScreen
+import com.cailloutr.rightnewscompose.ui.viewmodel.DetailsViewModel
+
+@RequiresApi(Build.VERSION_CODES.O)
+fun NavGraphBuilder.detailsNavGraph(
+    navigationActions: RightNewsNavigationActions,
+    viewModel: DetailsViewModel,
+    context: Context,
+    uriHandler: UriHandler,
+) {
+    navigation(
+        route = Graph.DETAILS,
+        startDestination = DetailsScreen.Details.route + "/{${Args.ARTICLE_ID}}"
+    ) {
+        composable(
+            route = DetailsScreen.Details.route + "/{${Args.ARTICLE_ID}}",
+            arguments = listOf(
+                navArgument(Args.ARTICLE_ID) {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            viewModel.getArticleById(
+                it.arguments?.getString(Args.ARTICLE_ID, "")?.fromRouteId() ?: ""
+            )
+            val uiState = viewModel.uiState.collectAsState()
+            DetailsScreen(
+                uiState = uiState.value,
+                navigateUp = { navigationActions.navigateUp() },
+                share = { url ->
+                    context.shareLinkIntent(
+                        title = R.string.share_link,
+                        value = url
+                    )
+                },
+                openLink = { url ->
+                    uriHandler.openUri(url)
+                },
+            )
+        }
+    }
+}
